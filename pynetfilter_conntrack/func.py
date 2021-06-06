3d654e96ea9a7aa52dcc4afaed0197bc2e17c6c9
Peter Honeder <peter.honeder@gmail.com>
2021-06-06 13:21:00 +0200

    fix: use correct 64-bit compatible types for the c pointer types (fixes segfault with alpine linux 3.10+)

--- pynetfilter_conntrack/func.py
+++ pynetfilter_conntrack/func.py
@@ -8,15 +8,13 @@ library = cdll.LoadLibrary(util.find_library("netfilter_conntrack"))
 #--------------------------------------------------------------------------
 # Define nf_conntrack_p and nfct_handle_p: pointer to fake structures
 #
-#class nf_conntrack(Structure):
-#    pass
-#class nfct_handle(Structure):
-#    pass
-#
-#nf_conntrack_p = POINTER(nf_conntrack)
-#nfct_handle_p = POINTER(nfct_handle)
-nf_conntrack_p = c_int
-nfct_handle_p = c_int
+class nf_conntrack(Structure):
+    pass
+class nfct_handle(Structure):
+    pass
+
+nf_conntrack_p = POINTER(nf_conntrack)
+nfct_handle_p = POINTER(nfct_handle)
 
 class nfct_conntrack_compare_t(Structure):
     _fields_ = (
